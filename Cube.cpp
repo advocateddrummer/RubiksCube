83a68e5f030dd752dbf2bf89c9d350d349a3ba1f
Ethan Alan Hereth <e.a.hereth@gmail.com>
2020-11-30 15:35:39 -0600

    Add documentation/comments to the u method

--- Cube.cpp
+++ Cube.cpp
@@ -13,15 +13,24 @@ Cube::Cube()
             { 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r'}};
 }
 
+/** \brief Perform a clockwise rotation of the up (u) face.
+ *
+ * This method rotates the upper face clockwise.
+ */
 void Cube::u()
 {
   std::cout << "Performing a \'u\' rotation..." << std::endl;
   std::cout << "Cube state before \'u\': " << std::endl;
   Print();
+  /* Make temporary copy of upper front edge. */
   std::vector<char> tmp = std::vector<char>(state[1].begin(), state[1].begin() + 3);
+  /* Move upper right edge to upper front. */
   std::copy(state[2].begin(), state[2].begin() + 3, state[1].begin());
+  /* Move upper back edge to upper right. */
   std::copy(state[3].begin(), state[3].begin() + 3, state[2].begin());
+  /* Move upper left edge to upper back. */
   std::copy(state[4].begin(), state[4].begin() + 3, state[3].begin());
+  /* Move upper front edge to upper left. */
   std::copy(tmp.begin(), tmp.begin() + 3, state[4].begin());
   std::cout << "\'u\' rotation complete" << std::endl;
   std::cout << "Cube state after \'u\': " << std::endl;
