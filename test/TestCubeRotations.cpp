82402b4327fb41c90a114c893d2d8892656e66e2
Ethan Alan Hereth <e.a.hereth@gmail.com>
2020-12-15 13:17:23 -0600

    Add suite of scrambled cube rotation tests
    This commit adds a test for each cube rotation on a scrambled state to
    make sure that the rotations do not alter the state of the cube just to
    make sure that the tests on solved cube states do not missing anything.

--- test/TestCubeRotations.cpp
+++ test/TestCubeRotations.cpp
@@ -1,6 +1,8 @@
 #include "gtest/gtest.h"
 #include "Cube.hpp"
 
+/* TODO: maybe use a gtest fixture here. */
+
 TEST(CubeRotationTest, x_equals_solved) {
   Cube a;
 
@@ -71,6 +73,114 @@ TEST(CubeRotationTest, zz_equals_solved) {
   a.zz();
 
   ASSERT_TRUE(a.IsSolved());
+}
+
+TEST(CubeRotationTest, x_does_not_change_state) {
+  Cube a;
+  Cube b;
+
+  a.RandomScramble();
+  b = a;
+
+  a.x();
+
+  ASSERT_EQ(a, b);
+}
+
+TEST(CubeRotationTest, xp_does_not_change_state) {
+  Cube a;
+  Cube b;
+
+  a.RandomScramble();
+  b = a;
+
+  a.xp();
+
+  ASSERT_EQ(a, b);
+}
+
+TEST(CubeRotationTest, xx_does_not_change_state) {
+  Cube a;
+  Cube b;
+
+  a.RandomScramble();
+  b = a;
+
+  a.xx();
+
+  ASSERT_EQ(a, b);
+}
+
+TEST(CubeRotationTest, y_does_not_change_state) {
+  Cube a;
+  Cube b;
+
+  a.RandomScramble();
+  b = a;
+
+  a.y();
+
+  ASSERT_EQ(a, b);
+}
+
+TEST(CubeRotationTest, yp_does_not_change_state) {
+  Cube a;
+  Cube b;
+
+  a.RandomScramble();
+  b = a;
+
+  a.yp();
+
+  ASSERT_EQ(a, b);
+}
+
+TEST(CubeRotationTest, yy_does_not_change_state) {
+  Cube a;
+  Cube b;
+
+  a.RandomScramble();
+  b = a;
+
+  a.yy();
+
+  ASSERT_EQ(a, b);
+}
+
+TEST(CubeRotationTest, z_does_not_change_state) {
+  Cube a;
+  Cube b;
+
+  a.RandomScramble();
+  b = a;
+
+  a.z();
+
+  ASSERT_EQ(a, b);
+}
+
+TEST(CubeRotationTest, zp_does_not_change_state) {
+  Cube a;
+  Cube b;
+
+  a.RandomScramble();
+  b = a;
+
+  a.zp();
+
+  ASSERT_EQ(a, b);
+}
+
+TEST(CubeRotationTest, zz_does_not_change_state) {
+  Cube a;
+  Cube b;
+
+  a.RandomScramble();
+  b = a;
+
+  a.zz();
+
+  ASSERT_EQ(a, b);
 }
 
 int main(int argc, char * argv[])
