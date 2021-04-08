bc04f909a665ab3073a49930d9e478bc710e2323
Ethan Alan Hereth <e.a.hereth@gmail.com>
2021-04-08 14:26:21 -0500

    Add a couple tests for SetCubeState
    This commit adds two tests (which I do not love) for the SetCubeState
    method. I wanted to test that it throws an exception when supplied with
    the incorrect number of cube elements but the EXPECT_THROW googletest
    macro is confusing. I am not certain that these tests are really
    complete nor that they even work the way I intend them to.

--- test/TestCube.cpp
+++ test/TestCube.cpp
@@ -60,6 +60,41 @@ TEST_F(CubeTest, SetCubeStateDefaultArg) {
   ASSERT_TRUE(a.IsSolved());
 }
 
+TEST_F(CubeTest, SetCubeStateCorrectNumOfArgs) {
+  try {
+    a.SetCubeState({ 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o',
+                     'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b',
+                     'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w', 'w',
+                     'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g', 'g',
+                     'y', 'y', 'y', 'y', 'y', 'y', 'y', 'y', 'y',
+                     'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r', 'r'});
+    ASSERT_TRUE(a.IsSolved());
+  } catch (const char * msg) {
+    FAIL() << "Did not expected an exception";
+  } catch (...) {
+    FAIL() << "Did not expected an exception";
+  }
+}
+
+TEST_F(CubeTest, SetCubeStateIncorrectNumOfArgs) {
+  /* TODO: I do not love this, all the examples I found for using EXPECT_THROW
+   * were confusing and I have settled on something like this which I am still
+   * not sure is really correct.
+   */
+  try {
+    a.SetCubeState({'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o',
+                    'b', 'b', 'o', 'r', 'b', 'b', 'b', 'b', 'b',
+                    'w', 'w', 'r', 'w', 'w', 'w', 'r', 'g', 'w',
+                    'g', 'y', 'y', 'y', 'y', 'y', 'y', 'y', 'y',
+                    'r', 'r', 'y', 'b', 'b', 'r', 'r', 'r', 'r'});
+    FAIL() << "Expected an exception";
+  } catch (const char * msg) {
+    EXPECT_STREQ( "Cube::SetCubeState must have an input state with 54 elements!", msg );
+  } catch (...) {
+    FAIL() << "Expected a different exception";
+  }
+}
+
 TEST_F(CubeTest, SetCubeStateSexyMove) {
   /* Use SetCubeState to set the cube state to the state after one 'sexy move'
    * has been performed.
