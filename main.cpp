af8b756e2b204b715cc414cfa9c5c95729b1913b
Ethan Alan Hereth <e.a.hereth@gmail.com>
2021-04-08 11:16:10 -0500

    Update main routine
    This commit simply adds a little logic in the main routine to scramble
    the cube, obtain a solution from kociemba using the Solve method and
    print the cube state.

--- main.cpp
+++ main.cpp
@@ -10,22 +10,17 @@ int main(int argc, char * argv[])
 
   Cube c;
 
-  if (c.IsSolved())
-    std::cout << "The cube is solved!!!" << std::endl;
-  else
-    std::cout << "The cube is NOT solved!!!" << std::endl;
+  c.RandomScramble();
+  c.Print();
+  const std::string solution = c.Solve();
+  c.ApplyTwists(solution);
+  c.Print();
 
-  c.u();
-  if (c.IsSolved())
-    std::cout << "The cube is solved!!!" << std::endl;
-  else
-    std::cout << "The cube is NOT solved!!!" << std::endl;
-
-  c.up();
-  if (c.IsSolved())
-    std::cout << "The cube is solved!!!" << std::endl;
-  else
-    std::cout << "The cube is NOT solved!!!" << std::endl;
-
-  return 0;
+  if (!c.IsSolved()) {
+    puts("NOT SOLVED...");
+    return EXIT_FAILURE;
+  } else {
+    puts("SOLVED!!!");
+    return 0;
+  }
 }
